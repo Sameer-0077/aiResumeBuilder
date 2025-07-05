03452fbef5f75f61f48f768088336d8f7ad23a40
Sameer-0077 <sameeryadav0077@gmail.com>
2025-07-05 14:31:39 +0530

    Created download layout component

---
+++ client/src/DownloadLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function DownloadLayout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+export default DownloadLayout;
