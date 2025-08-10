9d47e09da8d246112c4ab521f5f32f0e29015e84
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-11 00:57:33 +0530

    Added about-us and conatct routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,36 @@ import DownloadResume from "./pages/DownloadResume";
 import DownloadCoverLetter from "./pages/DownloadCoverLetter";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import useUserStore from "./store/userStore";
+import { useEffect } from "react";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
+  const setUser = useUserStore((state) => state.setUser);
+
+  const fetchUser = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/current-user", {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        lreturn;
+      } else {
+        setUser(data.user);
+        console.log(data.user);
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <div>
@@ -24,6 +52,8 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about-us" element={<About />} />
               <Route path="/download" element={<DownloadLayout />}>
                 <Route path="resume" element={<DownloadResume />} />
                 <Route path="cover-letter" element={<DownloadCoverLetter />} />
