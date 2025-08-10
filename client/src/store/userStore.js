c658fbae58301acfed3cf00d36094a4deaaba2de
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-11 01:02:28 +0530

    Add userStore

---
+++ client/src/store/userStore.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+const useUserStore = create((set) => ({
+  user: null,
+  setUser: (userData) => set({ user: userData }),
+  clear: () => set({ user: null }),
+}));
+
+export default useUserStore;
